864ce9f0a2301dbdf0b0c0c622bbf49e028251ee
VRXXCR <eycol12tily@gmail.com>
2026-05-07 05:48:24 -0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,8 +2,8 @@ import requests
 import feedparser
 import json
 
-WEBHOOK = "PEGA_AQUI_TU_WEBHOOK"
-RSS = "https://www.youtube.com/feeds/videos.xml?channel_id=TU_ID"
+WEBHOOK = "https://discord.com/api/webhooks/1501909697614319717/pruP2PZPl-u_q_g3VGF_5gcp8c8HVTNE1PJG2rjoCXgbD64RSr8_GXpKsOjQlZW2hHDC"
+RSS = "https://www.youtube.com/feeds/videos.xml?channel_id=UCk6mA2TlOmx1K3vbUa5DoPQ"
 
 feed = feedparser.parse(RSS)
 video = feed.entries[0]
@@ -13,28 +13,31 @@ title = video.title
 link = video.link
 thumb = f"https://img.youtube.com/vi/{video_id}/maxresdefault.jpg"
 
+# cargar estado
 try:
     with open("state.json", "r") as f:
         data = json.load(f)
 except:
     data = {"last": ""}
 
+# si es nuevo video
 if data["last"] != video_id:
 
     payload = {
-        "content": "# 🚨 Nuevo video ferfitos! @everyone",
+        "content": "# Nuevo Videos Ferfitos @everyone",
         "embeds": [
             {
                 "title": title,
                 "url": link,
                 "color": 16711680,
                 "image": {"url": thumb},
-                "footer": {"text": "Bot automático de YouTube"}
+                "footer": {"text": "YouTube Bot PRO • Auto Detect"}
             }
         ]
     }
 
     requests.post(WEBHOOK, json=payload)
 
+    # guardar nuevo estado
     with open("state.json", "w") as f:
         json.dump({"last": video_id}, f)
